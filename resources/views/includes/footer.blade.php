f3ee6d54da984236f34daf328e6383429db75104
virtu-alex <alessio.cordari@gmail.com>
2022-09-15 19:16:22 +0200

    feat: major update (completed)

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,76 @@
 <footer>
-
+    <div class="container position-relative">
+        <div class="row text-white ">
+            <div class="col-2">
+                <ul>
+                    <li><strong>DC COMICS</strong></li>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <ul>
+                    <li><strong>SHOP</strong></li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li><strong>DC</strong></li>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy (new)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscription</li>
+                    <li>Talents Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li><strong>SITES</strong></li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <img class="dc-bg" src="{{ asset('images/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+    <section class="bottom-footer">
+        <div class="container h-100">
+            <div class="row align-items-center h-100">
+                <div class="col-7">
+                    <button class="sign-up">
+                        SIGN-UP NOW
+                    </button>
+                </div>
+                <div class="col-5 d-flex">
+                    <div>
+                        <h3 class="text-primary">FOLLOW US</h3>
+                    </div>
+                    <div class="px-2"><img src="{{ asset('/images/footer-facebook.png') }}" alt="footer-facebook">
+                    </div>
+                    <div class="px-2"><img src="{{ asset('/images/footer-twitter.png') }}" alt="footer-twitter"></div>
+                    <div class="px-2"><img src="{{ asset('/images/footer-youtube.png') }}" alt="footer-youtube"></div>
+                    <div class="px-2"><img src="{{ asset('/images/footer-pinterest.png') }}" alt="footer-pinterest">
+                    </div>
+                    <div class="px-2"><img src="{{ asset('/images/footer-periscope.png') }}" alt="footer-periscope">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -12,11 +12,37 @@ $series = config('comics');
             @endforeach
         </div>
         <div class="row mt-4">
-            <div class="col d-flex justify-content-center">
+            <div class="col d-flex justify-content-center mb-3">
                 <button class="btn btn-primary">
                     <a href="#" class="text-white">LOAD MORE</a>
                 </button>
             </div>
         </div>
     </div>
+    <section id="digitals" class="bg-primary">
+        <div class="container">
+            <div class="row py-3 text-white">
+                <div class="col">
+                    <img src="{{ asset('/images/digitals/buy-comics-digital-comics.png') }}" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div class="col">
+                    <img src="{{ asset('/images/digitals/buy-comics-merchandise.png') }}" alt="">
+                    <span>DC MERCHANDISE</span>
+                </div>
+                <div class="col">
+                    <img src="{{ asset('/images/digitals/buy-comics-subscriptions.png') }}" alt="">
+                    <span>SUBSRIPTION</span>
+                </div>
+                <div class="col">
+                    <img src="{{ asset('/images/digitals/buy-comics-shop-locator.png') }}" alt="">
+                    <span>SHOP LOCATOR</span>
+                </div>
+                <div class="col">
+                    <img src="{{ asset('/images/digitals/buy-dc-power-visa.svg') }}" alt="">
+                    <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
+    </section>
 </section>
